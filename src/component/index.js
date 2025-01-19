714556ce28ef3a0f1b81deb8bf77212b3d92c42a
Shengwei Zhu <zhu.shengwei35@gmail.com>
2025-01-19 11:40:56 -0800

    Updated index.js in component directory

---
+++ src/component/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='header'>
+      <div className='header_logo'>
+        <img src={GDriveLogo} alt=''/>
+        <span>Drive</span>
+      </div>
+      <div className='header_searchContainer'>
+        
+      </div>
+      <div className='header_icons'></div>
+    </div>
+  )
+}
+
+export default index
