8a2208fbdeeef790ce70ab7142c7b5141feeb829
Shengwei Zhu <zhu.shengwei35@gmail.com>
2025-01-19 15:19:10 -0800

    Updated index.js

--- src/component/sidebar/index.js
+++ src/component/sidebar/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import NewFile from './NewFile'
+import SidebarItem from './SidebarItem'
+
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import ImportantDevicesIcon from '@material-ui/icons/ImportantDevices';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import StorageIcon from '@material-ui/icons/Storage';
 
 const index = () => {
     return (
@@ -7,9 +16,13 @@ const index = () => {
             <NewFile />
 
             <div className="sidebar_items">
-                {/* sidebar items */}
-
-
+                <SidebarItem arrow icon={(<InsertDriveFileIcon />)} label={'My Drive'} />
+                <SidebarItem arrow icon={(<ImportantDevicesIcon />)} label={'Computers'} />
+                <SidebarItem icon={(<PeopleAltIcon />)} label={'Shared with me'} />
+                <SidebarItem icon={(<QueryBuilderIcon />)} label={'Recent'} />
+                <SidebarItem icon={(<StarBorderIcon />)} label={'Starred'} />
+                <SidebarItem icon={(<DeleteOutlineIcon />)} label={'Bin'} />
+                
                 <hr/>
 
                 {/* sidebar elements */}
