495e392bb8053f00f745cbcc0ffb842ff4b78027
Shengwei Zhu <zhu.shengwei35@gmail.com>
2025-01-27 12:58:44 -0800

    test

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDFQy9IqZsbAC21zrnxqu38ucxDkrmRb_4",
+    authDomain: "drive-clone-e3a24.firebaseapp.com",
+    projectId: "drive-clone-e3a24",
+    storageBucket: "drive-clone-e3a24.firebasestorage.app",
+    messagingSenderId: "611476625829",
+    appId: "1:611476625829:web:624eabddb49eb88e48f04c",
+    measurementId: "G-17LZ68D8HZ"
+  };
