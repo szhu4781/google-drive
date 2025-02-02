3dd7507817bcf05a6e6708d1aeb5574c722bb455
Shengwei Zhu <zhu.shengwei35@gmail.com>
2025-02-01 22:38:41 -0800

    Revert "Updated firebase.js"
    This reverts commit ad659abc7520d01679287befbbe54dd49cdb5713.

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import 'firebase/compat/storage';
 import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -17,6 +18,7 @@ const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
+const storage = firebase.storage()
 const database = firebaseApp.firestore()
 
-export { auth, provider, database }
+export { auth, provider, database, storage }
