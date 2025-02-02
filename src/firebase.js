ad659abc7520d01679287befbbe54dd49cdb5713
Shengwei Zhu <zhu.shengwei35@gmail.com>
2025-02-01 17:19:48 -0800

    Updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDFQy9IqZsbAC21zrnxqu38ucxDkrmRb_4",
@@ -7,4 +12,13 @@ const firebaseConfig = {
     messagingSenderId: "611476625829",
     appId: "1:611476625829:web:624eabddb49eb88e48f04c",
     measurementId: "G-17LZ68D8HZ"
-  };
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+const storage = firebase.storage()
+const database = firebaseApp.firestore()
+
+export { auth, provider, database, storage }
