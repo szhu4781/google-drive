424721fa52accca15b7780472c7ca8915251e404
Shengwei Zhu <zhu.shengwei35@gmail.com>
2025-02-01 20:14:23 -0800

    1

---
+++ src/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from 'cloudinary'
+
+cloudinary.config({
+    cloud_name: 'dvjsrqfrq', 
+    api_key: '514751293496615',       
+    api_secret: 'AiumYF12ghNUwEivEI-pGTKjj5M', 
+  });
+  
+  export default cloudinary;
---
+++ src/component/filesView/FilesView.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FilesView = () => {
+  return (
+    <div className='fileView'>
+      <div className="fv_row">
+
+      </div>
+      <div className="fv_title">
+        <div className="fv_title_left">
+            <p>Name</p>
+        </div>
+        <div className="fv_title_right">
+            <p>Last Modified</p>
+            <p>File Size</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FilesView
