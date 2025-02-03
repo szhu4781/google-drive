9880eba2da0a453600bea96d971031cfe3fbc3e3
Shengwei Zhu <zhu.shengwei35@gmail.com>
2025-02-02 21:20:13 -0800

    Updated FilesView.js

--- src/component/filesView/FilesView.js
+++ src/component/filesView/FilesView.js
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../../styles/FilesView.css'
+import supabase from '../../supabase';
+import FileItem from './FileItem';
 
 const FilesView = () => {
-  return (
-    <div className='fileView'>
-      <div className="fv_row">
+    const [files, setFiles] = useState([]);
 
-      </div>
-      <div className="fv_title">
-        <div className="fv_title_left">
-            <p>Name</p>
+    useEffect(() => {
+        //Fetch data from Supabase realtime
+        const fetchFile = async () => {
+            try {
+                const {data, error} = await supabase
+                    .from('files')
+                    .select('*');
+
+                if(error) throw error;
+
+                console.log('Fetched files from Supabase', data)
+
+                //Store file data
+                setFiles(data);
+
+            } catch(error){
+                console.log('Error fetching files: ', error.message);
+            }
+        };
+        fetchFile();
+    }, [])
+
+    console.log(files)
+
+    return (
+        <div className='fileView'>
+            <div className="fv_row">
+            </div>
+            <div className="fv_title">
+                <div className="fv_title_left">
+                    <p>Name</p>
+                </div>
+                <div className="fv_title_right">
+                    <p>Last Modified</p>
+                    <p>File Size</p>
+                </div>
+            </div>
+            {
+                //Map the files
+                files.map(({id, caption, timestamp, fileUrl, size}) => (
+                    <FileItem key={id} id={id} caption={caption} timestamp={timestamp} fileUrl={fileUrl} size={size} />
+                ))
+            }
         </div>
-        <div className="fv_title_right">
-            <p>Last Modified</p>
-            <p>File Size</p>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default FilesView
